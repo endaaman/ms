efb21683706493e1d0c7d456dcd2166c0ff94a26
endaaman <buhibuhidog@gmail.com>
2014-06-25 02:02:18 +0900

    commit

--- template/urls.py
+++ template/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from views import *
 
+v = AngularTemplateView.as_view
+
 urlpatterns = (
-    url(r'^home.html$', AngularTemplateView.as_view(template_name='home.haml')),
-    url(r'^candidates.html$', AngularTemplateView.as_view(template_name='candidates.haml')),
-    url(r'^candidates-each.html$', AngularTemplateView.as_view(template_name='candidates-each.haml')),
-    url(r'^contest.html$', AngularTemplateView.as_view(template_name='contest.haml')),
-    url(r'^vote.html$', AngularTemplateView.as_view(template_name='vote.haml')),    
+    url(r'^home.html$', v(template_name='home.haml')),
+    url(r'^candidates.html$', v(template_name='candidates.haml')),
+    url(r'^candidates-each.html$', v(template_name='candidates-each.haml')),
+    url(r'^contest.html$', v(template_name='contest.haml')),
+    url(r'^vote.html$', v(template_name='vote.haml')),
 )
